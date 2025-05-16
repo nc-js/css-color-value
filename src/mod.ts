eb244be09aa310b3ac3a6a7204eb155f36701a48
Samantha <contact@samanthanguyen.me>
2025-05-16 02:35:39 -0500

    release: v0.2.1 (#14)

--- src/mod.ts
+++ src/mod.ts
@@ -1,8 +1,3 @@
-/**
- * The polyfill API for `CSSColorValue` and its related types.
- * @module
- */
-
 export * from './css-color-value.ts'
 export * from './matches.ts'
 export * from './types.ts'
